688fd97fa39a2d38326d3bd8270cfb02be7bcbbc
Jean PAUPHILET <jpauph@mit.edu>
2017-09-04 12:05:46 -0400

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
-using SubsetSelection
-using Base.Test
+using SubsetSelection, StatsBase, Base.test
 
-# write your own tests here
-@test 1 == 2
+n = 500; p = 1000; k = 10;
+
+indices = sort(sample(1:p, StatsBase.Weights(ones(p)/p), k, replace=false));
+w = sample(-1:2:1, k);
+X = randn(n,p); Y = X[:,indices]*w;
+Sparse_Regressor = subsetSelection(OLS(), Constraint(k), Y, X)
+for i in 1:k
+  @test Sparse_Regressor.indices[i]==indices[i]
+end
